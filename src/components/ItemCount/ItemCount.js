29162796d0c047b3f06298e45f735c91d5099d5c
Ivan Telleria <ivantelleria96@gmail.com>
2023-01-14 16:09:12 +0100

    Agrego archivo json temporal

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,9 +1,31 @@
 import "./itemCount.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+
+const datos = [
+    {id:1, name: "A", size: 32},
+    {id:2, name: "B", size: 46},
+    {id:3, name: "C", size: 34},
+    {id:4, name: "D", size: 30},
+    {id:5, name: "E", size: 42},
+]
 
 const ItemCount = () => {
     const [contador, setContador] = useState(1);
+    const [products, setProducts] = useState([]);
     let stock = 5;
+
+    const getProducts = new Promise((resolve)=>{
+        setTimeout(()=> resolve(datos), 2000)
+    })
+    useEffect(()=>{
+        getProducts
+        .then((response)=>{
+            setProducts(response)
+        })
+        .catch((error)=> console.error(error))
+    },[])
+
+
     const sumar = () =>{
         if(contador === stock){
             return
@@ -20,6 +42,14 @@ const ItemCount = () => {
     }
   return (
     <div className='counter'>
+       <ul>
+       {
+            products.map((item)=>(
+                <li key={item.id}>{item.name}</li>
+            ))
+        }
+       </ul>
+        
         <div className='controller'>
             <button onClick={restar} id="menos">-</button>
             <div>
@@ -30,7 +60,6 @@ const ItemCount = () => {
         <div>
             <button>Agregar al carrito</button>
         </div>
-
     </div>
   )
 }
