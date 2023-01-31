b65e6b4e264e4a915a575d2bcd0f5c63f04c4d81
Ivan Telleria <ivantelleria96@gmail.com>
2023-01-31 17:45:20 +0100

    añadimos validacion para comprobar si los a agregar ya se encuentran en el carrito

--- src/context/CartProvider.js
+++ src/context/CartProvider.js
@@ -1,32 +1,43 @@
-import { useState } from "react"
-import { CartContext } from "./cartContext"
+import { useState } from "react";
+import { CartContext } from "./cartContext";
 
-const CartProvider = ({children}) => {
-  let [cart, setCart] = useState([])
+const CartProvider = ({ children }) => {
+  let [cart, setCart] = useState([]);
 
-  const addToCart = (item, quantity) =>{
-    const newProd = {
-      name: item.title,
-      price: item.price,
-      category: item.category,
-      quantity: quantity
+  const addToCart = (item, quantity) => {
+    let prodID = item.id
+    console.log(prodID)
+    let existe = cart.some((prod) => prod.id === prodID);
+    console.log(existe)
+    if (existe) {
+      let carrito = cart.find((prod)=> prod.id === prodID ? prod.quantity += quantity : prod.quantity)
+      setCart([carrito])
+    } else {
+      const newProd = {
+        id: item.id,
+        name: item.title,
+        price: item.price,
+        category: item.category,
+        quantity: quantity,
+      };
+      setCart([...cart, newProd]);
     }
-    setCart([...cart, newProd])
-  }
+    console.log(cart)
+  };
 
-  const clear =()=> setCart([])
-  
-  const removeItem = (id) =>{
+  const clear = () => setCart([]);
+
+  const removeItem = (id) => {
     let prodID = id;
-    let newCart = cart.filter(item=> item.id !== prodID)
-    setCart([...newCart])
-  }
+    let newCart = cart.filter((item) => item.id !== prodID);
+    setCart([...newCart]);
+  };
 
   return (
-    <CartContext.Provider value={{addToCart, clear, removeItem}}>
-        {children}
+    <CartContext.Provider value={{ addToCart, clear, removeItem }}>
+      {children}
     </CartContext.Provider>
-  )
-}
+  );
+};
 
-export default CartProvider
+export default CartProvider;
